a2e1ae05441e8a058c44d2e85ddc05345811a02f
ths <alexandre.urbaniak01@gmail.com>
2018-06-10 18:21:58 +0200

    install jquery and webpack

--- flyaround/webpack.config.js
+++ flyaround/webpack.config.js
@@ -4,6 +4,7 @@ Encore
     .setOutputPath('web/build/')
     .setPublicPath('/web')
     .addEntry('main', './assets/scss/main.scss')
+    .addEntry('app', './assets/js/app.js')
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSassLoader()
